5ee7da7ec9163e76777e4e8823d92dba9930d792
Lukas <lukasapplefan@gmail.com>
2020-02-26 18:19:44 +0100

    Initial Commit

---
+++ Sources/MySexyPackage/BigSexyService.swift
@@ -0,0 +1,16 @@
+//
+//  BigSexyService.swift
+//  MyCurrentApp
+//
+//  Created by Lukas Mauffré on 26/02/2020.
+//  Copyright © 2020 Lukas Mauffré. All rights reserved.
+//
+
+import Foundation
+
+public struct BigSexyService {
+    
+    public static func doSomethingSpecial() {
+        print("🎊 Something special happened 🎉")
+    }
+}
